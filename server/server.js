db633fd377cdff7bd9018e8e8755b235ea50d1cd
10000highfives <jared.a.fowler@gmail.com>
2016-02-19 17:54:01 -0800

    Modifies middleware to match new controller

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const controller = require('./controller');
-
+const fileMakerController = require('./fileMakerController');
+const bodyParser = require('body-parser');
 
 app.use(express.static(path.join(__dirname, './../')));
+app.use(bodyParser.json());
 
 //route to index on root path
 //this might change depending on how React Router is setup
@@ -12,6 +13,8 @@ app.get('/', (req, res) => {
 	res.send('index.html');
 });
 
-app.post('/gulp', controller.makeGulp);
+app.get('/gulp', fileMakerController.createFile);
 
-app.listen(3000); 
+app.listen(3000, () => {
+	console.log('listening on 3000');
+});
