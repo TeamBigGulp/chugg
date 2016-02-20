7a8e7ddde05491554bedff44e4c520d81e20d42f
David Schwartz <dbs2162@columbia.edu>
2016-02-20 13:24:37 -0800

    getting setup for zip

--- server/server.js
+++ server/server.js
@@ -3,18 +3,17 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const fileMakerController = require('./fileMakerController');
-const responseController = require('./responseController');
 
 app.use(express.static(path.join(__dirname, './../')));
 app.use(bodyParser.text());
 
 
 //route to index on root path
-//this might change depending on how React Router is setup
 app.get('/', (req, res) => {
 	res.send('index.html');
 });
 
+// post request to get the zipped version of the documents that were created
 app.post('/gulp', fileMakerController.createsFile, fileMakerController.zipsFile);
 
 app.listen(3000, function() {
