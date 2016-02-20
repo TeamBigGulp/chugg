aa230d185126e1c5023897f04e8efbe6eb28509f
10000highfives <jared.a.fowler@gmail.com>
2016-02-19 18:10:03 -0800

    fix merge conflicts

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -6,7 +5,8 @@ const fileMakerController = require('./fileMakerController');
 const bodyParser = require('body-parser');
 
 app.use(express.static(path.join(__dirname, './../')));
-app.use(bodyParser.json());
+app.use(bodyParser.text());
+
 
 //route to index on root path
 //this might change depending on how React Router is setup
@@ -14,30 +14,9 @@ app.get('/', (req, res) => {
 	res.send('index.html');
 });
 
-app.get('/gulp', fileMakerController.createFile);
-
-app.listen(3000, () => {
-	console.log('listening on 3000');
-=======
-var express = require('express');
-var path = require('path');
-var app = express();
-var bodyParser = require('body-parser');
-
-app.use(bodyParser.text());
-
-app.use(express.static(path.join(__dirname, './../')));
-
-app.get('/', function(req,res) {
-  res.sendFile('/index.html');
-});
-
-app.post('/gulp', function(req, res) {
-  console.log(req.body);
-  res.end();
-})
+app.post('/gulp', fileMakerController.createFile);
 
 app.listen(3000, function() {
   console.log('Server is listening on port 3000');
->>>>>>> master
+
 });
