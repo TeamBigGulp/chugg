78510b095de3695633f4a98aefd68a3a7e899ff5
hamzasurti <hamza.surti1@gmail.com>
2016-02-20 16:13:07 -0800

    styles added

--- components/Gulpview.js
+++ components/Gulpview.js
@@ -1,13 +1,23 @@
 var React = require('react');
+var Codemirror = require('react-codemirror');
 
+require('codemirror/mode/javascript/javascript');
+require('codemirror/mode/xml/xml');
+require('codemirror/mode/markdown/markdown');
 
 
 var Gulpview = React.createClass({
   render: function () {
+    var options = {
+            lineNumbers: true,
+            mode: 'javascript'
+        };
     return (
       <div id='Gulpview'>
-        Gulpview
-
+<Codemirror value={this.props.value} onChange={this.props.updateCode} options={options} />
+<form id="download-files" onSubmit={this.props.postRequest}>
+<input type="submit" value="Download Files" />
+</form>
       </div>
     )
   }
