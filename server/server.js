6354c4bfde5c9b9683d16ef8d1b249309dcecdae
Isaac Durand <isaacdurand@Isaacs-MacBook-Air.local>
2016-02-25 10:27:37 -0800

    Saving my work before I pull from upstream - login still works

--- server/server.js
+++ server/server.js
@@ -1,40 +1,41 @@
+// I'm modeling my code on http://mherman.org/blog/2015/01/31/local-authentication-with-passport-and-express-4/#.Vs5DW5MrKgS. 
+// I also found http://mongoosejs.com/docs/index.html and http://mongoosejs.com/docs/guide.html helpful.
+
+// Dependencies
 const express = require('express');
 const app = express();
 const path = require('path');
+// const favicon = require('serve-favicon'); // I don't think we need this.
+const logger = require('morgan'); // Is this what's causing everything to be logged in the terminal?
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 const fileMakerController = require('./fileMakerController');
+// const request = require('superagent'); // We were using this for resting, but I don't think we need it anymore. We're not using 'request' anywhere in this file.
 
-var request = require('superagent'); // for testing
+// We don't need to save variables called 'routes' or 'users'.
 
-// See http://mongoosejs.com/docs/index.html. This 'getting started' page is useful, but the API docs aren't. Use this guide instead: http://mongoosejs.com/docs/guide.html
-// I'd like some clarification on exactly what these two lines of code are doing
+// We're not using a view enginge.
 
-// Now I'm modeling my code on http://mherman.org/blog/2015/01/31/local-authentication-with-passport-and-express-4/#.Vs5DW5MrKgS
-
-// var favicon = require('serve-favicon'); // I don't think I need this.
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-
-var mongoose = require('mongoose');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-
-app.use(logger('dev'));
+// This is where we'd use favicon.
+app.use(logger('dev')); // This is logging our GET and POST requests in the terminal.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text()); // Do I need all of these bodyParsers?
 app.use(cookieParser());
-app.use(require('express-session')({
-	secret: 'keyboard cat',
+app.use(session({
+	secret: 'chugg',
 	resave: false,
 	saveUninitialized: false
-}));
+})); // I'm not sure what this is doing. See https://github.com/expressjs/session
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, './../')));
 
-// app.use('/', routes); // Here, routes was referring to a specific project folder. I don't think I need this line.
-
+// ROUTES
 //route to index on root path
 app.get('/', (req, res) => {
 	res.send('index.html');
@@ -71,50 +72,14 @@ app.listen(3000, function() {
   console.log('Server is listening on port 3000');
 });
 
-// passport config
+// Configure Passport (NEED TO REVIEW THIS)
 var Account = require('./../models/account');
 passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
 
-// mongoose
+// Mongoose - UPDATE URI?
 mongoose.connect('mongodb://localhost/passport_local_mongoose_express4');
-// mongoose.connect('mongodb://localhost/test'); // Open a connection to the 'test' database
-
-// Here's what I'm doing to save users to the database:
- 
-/*
-var db = mongoose.connection; // Grab onto the connection we just opened. (Think of the connection as the database.)
-// Handle errors?
-
-
-db.once('open', function() { // Where does the .once method come from?
-	
-	var userSchema = mongoose.Schema({
-		username: String,
-		password: String
-	});
-
-	var User = mongoose.model('User', userSchema);
-
-	// Save a new user in two steps
-	// var isaac = new User({
-	// 	name: 'Isaac',
-	// 	password: 'test'
-	// });
-	// console.log('The name of the user we just created is', isaac.name);
-	// isaac.save(function(err) {
-	// 	if (err) console.log(err); // return before cl?
-	// })
-
-	// Save a new user in one step
-	// User.create({username: 'test', password: 'test'});
-
-	User.find(function(err, users) {
-		console.log('All our users:', users);
-	})
-})
-*/
 
 // Need catch 404 / error handlers
 
